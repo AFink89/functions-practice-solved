ce61ec81d64bfa93e21dfd786e0b810313fc7802
AMaupin <alexandrafink8989@gmail.com>
2025-08-25 09:26:59 -0700

    created new functions using arrow function syntax, comment out original examples

--- exercises/e4.js
+++ exercises/e4.js
@@ -9,10 +9,13 @@
  * splitFirstAndLastNames('John Smith') => ['John', 'Smith']
  */
 
-function splitFirstAndLastNames(str) {
-  return str.split(' ');
-}
+// function splitFirstAndLastNames(str) {
+//  return str.split(' ');
+// }
 
+const splitFirstAndLastNames = (string) => string.split(' ');
+
+console.log(splitFirstAndLastNames('John Smith'));
 
 /** =========================
  * The personAge() constant is assigned to a function, that takes an object of person Data as the argument.
@@ -20,10 +23,15 @@ function splitFirstAndLastNames(str) {
  * personAge({ id: 1, name: 'Someone', age: 32}) => 32
  */
 
-const personAge = function(personObject) {
-  return personObject.age;
-}
+// const personAge = function (personObject) {
+//  return personObject.age;
+// }
 
+const personAge = (personObject) => {
+  return personObject.age;
+};
+
+console.log(personAge({ id: 1, name: 'Someone', age: 32 }));
 
 /** =========================
  * The isNameInArray function takes an array and string of name as the arguments.
@@ -32,9 +40,16 @@ const personAge = function(personObject) {
  * isNameInArray(['Jon', 'Michael', 'Andrey'], 'James') => false;
  */
 
-const isNameInArray = function(arr, name) {
-  return arr.includes(name);
+// const isNameInArray = function (arr, name) {
+//  return arr.includes(name);
+// }
+
+const isNameInArray = (array, name) => {
+  return array.includes(name);
 }
+
+console.log(isNameInArray(['Jon', 'Michael', 'Andrey'], 'Michael'));
+console.log(isNameInArray(['Jon', 'Michael', 'Andrey'], 'James'));
 
 
 /** =========================
@@ -44,9 +59,9 @@ const isNameInArray = function(arr, name) {
  * !!! Hint: There are two functions to refactor !!!
  */
 
-const logSecondsUpToMax = function(max) {
+/** const logSecondsUpToMax = function (max) {
   let i = 0;
-  const timer = setInterval(function() {
+  const timer = setInterval(function () {
     if (i < max) {
       console.log(++i);
     } else {
@@ -54,6 +69,21 @@ const logSecondsUpToMax = function(max) {
     }
   }, 1000);
 }
+  */
+
+const logSecondsUpToMax = (max) => {
+  let i = 0;
+  const timer = setInterval(() => {
+    if (i < max) {
+      console.log(++i);
+    } else {
+      clearInterval(timer);
+    }
+  }, 1000);
+  return timer;
+};
+
+
 
 
 
