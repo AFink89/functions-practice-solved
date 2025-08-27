c2a28d5ec31afa7a9b24964ba5d9e11f09b19ab1
AMaupin <alexandrafink8989@gmail.com>
2025-08-27 08:56:28 -0700

    added function for totalRoomsArea in e2.js

--- exercises/e2.js
+++ exercises/e2.js
@@ -4,14 +4,13 @@
 /**
  * ====================================================
  * Create a function that works as follows:
-   * The function name is 'rectArea'
-   * The function returns the value of the area of a rectangle (sideA * sideB)
-   * The function takes 2 parameters (two sides) to calculate the area from
+ * The function name is 'rectArea'
+ * The function returns the value of the area of a rectangle (sideA * sideB)
+ * The function takes 2 parameters (two sides) to calculate the area from
  * rectArea(3, 2) => 6
-*/
+ */
 
 // Your function code starts here...
-
 function rectArea(sideA, sideB) {
   return sideA * sideB;
 }
@@ -21,16 +20,15 @@ console.log(rectArea(3, 2));
 // Use the function you created above to calculate the areas of 3 rooms
 // Assign the results to room1, room2, and room3 variables
 // Data for calculation: room1: 4 by 6, room2: 13 by 3, room3: 4 by 8
-
-// Your code goes here:
-
 let room1 = rectArea(4, 6);
 let room2 = rectArea(13, 3);
 let room3 = rectArea(4, 8);
 
-console.log("Room 1 Area: ", room1);
-console.log("Room 2 Area: ", room2);
-console.log("Room 3 Area: ", room3);
+function totalRoomsArea(room1, room2, room3) {
+  return room1 + room2 + room3;
+}
+
+console.log(totalRoomsArea(room1, room2, room3));
 
 
 /**
@@ -42,30 +40,17 @@ console.log("Room 3 Area: ", room3);
  * totalRoomsArea(area1, area2, area3) => Number
  * totalRoomsArea(24, 35, 24) => 83
  */
-
-// Your code goes here...
-
 function totalRoomsArea(area1, area2, area3) {
   return area1 + area2 + area3;
 }
 
-let area1 = 24;
-let area2 = 35;
-let area3 = 24;
-
-console.log("The total area of all 3 rooms: ", totalRoomsArea(area1, area2, area3));
-
-
-// Make the function call in the parentheses of the console.log statement.
-// Use room1, room2, room3 variables as arguments for the function call
-/* console.log("The total area of all 3 rooms:", roomArea(room1, room2, room3)); */
-
-
+// ✅ Define the result so it exists when referenced later
+const result = totalRoomsArea(room1, room2, room3);
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-2"
 // If the test has all tests passed, switch to the next exercise file
-// If any of the tests fails, refactor the code and run the test command after you've fixed the function
+// If any of the tests fails,
 
 
 // Do not delete or change the lines beneath
